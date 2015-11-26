0531e8b37bf48c79f7587731321384b803309e00
U-DESKTOP-LSP80NP\mirag <mirage.patel@gmail.com>
2015-11-25 22:29:28 -0500

    Login page merged with persist sql example, shows increasing # of Michaels

--- loginpage.hs
+++ loginpage.hs
@@ -1,66 +1,66 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
-import           Control.Applicative ((<$>), (<*>))
-import           Data.Text           (Text)
-import           Data.Time           (Day)
-import           Yesod
-import           Yesod.Form.Jquery
-import           Database.Persist.Sqlite
-import           Control.Monad.Trans.Resource (runResourceT)
-import           Control.Monad.Logger (runStderrLoggingT)
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE ViewPatterns               #-}
+import Yesod
+import Yesod.Form.Jquery
+import Data.Text (Text)
+import Database.Persist.Sqlite
+import Control.Monad.Trans.Resource (runResourceT)
+import Control.Monad.Logger (runStderrLoggingT)
 
 -- Define our entities as usual
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Person
-    username String
-    password String
-	age      Int
+    firstName String
+    lastName String
+    age Int
     deriving Show
 |]
 
 -- We keep our connection pool in the foundation. At program initialization, we
 -- create our initial pool, and each time we need to perform an action we check
 -- out a single connection from the pool.
-data App = App ConnectionPool
--- data App = App
+data PersistTest = PersistTest ConnectionPool
 
-mkYesod "App" [parseRoutes|
-/         HomeR GET
+-- We'll create a single route, to access a person. It's a very common
+-- occurrence to use an Id type in routes.
+mkYesod "PersistTest" [parseRoutes|
+/ HomeR GET
 /register RegisterR GET
+/registered AccRegisterR POST
 /person/#PersonId PersonR GET
-/person   PersonR POST
+/login   LoginR POST
 |]
 
-instance Yesod App
-
+-- Nothing special here
+instance Yesod PersistTest
 -- Now we need to define a YesodPersist instance, which will keep track of
 -- which backend we're using and how to run an action.
-instance YesodPersist App where
-    type YesodPersistBackend App = SqlBackend
-    runDB action = do
-        App pool <- getYesod
-        runSqlPool action pool
 
--- Tells our application to use the standard English messages.
--- If you want i18n, then you can supply a translating function instead.
-instance RenderMessage App FormMessage where
+instance YesodJquery PersistTest
+
+instance RenderMessage PersistTest FormMessage where
     renderMessage _ _ = defaultFormMessage
 
--- And tell us where to find the jQuery libraries. We'll just use the defaults,
--- which point to the Google CDN.
-instance YesodJquery App
+instance YesodPersist PersistTest where
+    type YesodPersistBackend PersistTest = SqlBackend
+    runDB action = do
+        PersistTest pool <- getYesod
+        runSqlPool action pool
 
--- The datatype we wish to receive from the login form
-data Person = Person
+data User = User
     { username      :: Text
     , password      :: Text
     }
   deriving Show
 
--- The datatype we wish to receive from the registration form
 data NewPerson = NewPerson
     { newUsername         :: Text
     , newPassword         :: Text
@@ -68,29 +68,8 @@ data NewPerson = NewPerson
     }
   deriving Show
 
--- Declare the form. The type signature is a bit intimidating, but here's the
--- overview:
---
--- * The Html parameter is used for encoding some extra information. See the
--- discussion regarding runFormGet and runFormPost below for further
--- explanation.
---
--- * We have our Handler as the inner monad, which indicates which site this is
--- running in.
---
--- * FormResult can be in three states: FormMissing (no data available),
--- FormFailure (invalid data) and FormSuccess
---
--- * The Widget is the viewable form to place into the web page.
---
--- Note that the scaffolded site provides a convenient Form type synonym,
--- so that our signature could be written as:
---
--- > personForm :: Form Person
---
--- For our purposes, it's good to see the long version.
-personForm :: Html -> MForm Handler (FormResult Person, Widget)
-personForm = renderDivs $ Person
+userForm :: Html -> MForm Handler (FormResult User, Widget)
+userForm = renderDivs $ User
     <$> areq textField     "Username: " Nothing
     <*> areq passwordField "Password: " Nothing
 
@@ -100,38 +79,24 @@ newAccountForm = renderDivs $ NewPerson
     <*> areq passwordField "Password: " Nothing
     <*> areq passwordField "Confirm: " Nothing
 
--- The GET handler displays the form
+-- List all people in the database
 getHomeR :: Handler Html
 getHomeR = do
-    -- Generate the form to be displayed
+    (widget, enctype) <- generateFormPost userForm
     people <- runDB $ selectList [] [Asc PersonAge]
-    (widget, enctype) <- generateFormPost personForm
-	defaultLayout
+    defaultLayout
         [whamlet|
             <h1>Login to Pong Web App</h1>
             <a href=@{RegisterR}>Click to register for an acount!
             <p>
                 Login with your username and password.
-                <form method=post action=@{PersonR} enctype=#{enctype}>
+                <form method=post action=@{LoginR} enctype=#{enctype}>
                     ^{widget}
                     <button>Login
-			<p>
-			<ul>
+            <ul>
                 $forall Entity personid person <- people
                     <li>
                         <a href=@{PersonR personid}>#{personFirstName person}
-        |]
-
-getRegisterR :: Handler Html
-getRegisterR = do
-    -- Generate the form to be displayed
-    (widget, enctype) <- generateFormPost newAccountForm
-    defaultLayout
-        [whamlet|
-            <h1>Register to Pong Web App</h1>
-            <form method=post action=@{PersonR} enctype=#{enctype}>
-                ^{widget}
-                <button>Register
         |]
 
 -- We'll just return the show value of a person, or a 404 if the Person doesn't
@@ -141,29 +106,54 @@ getPersonR personId = do
     person <- runDB $ get404 personId
     return $ show person
 
+getRegisterR :: Handler Html
+getRegisterR = do
+    -- Generate the form to be displayed
+    (widget, enctype) <- generateFormPost newAccountForm
+    defaultLayout
+        [whamlet|
+            <h1>Register to Pong Web App</h1>
+            <form method=post action=@{AccRegisterR} enctype=#{enctype}>
+                ^{widget}
+                <button>Register
+        |]
+
 -- The POST handler processes the form. If it is successful, it displays the
 -- parsed person. Otherwise, it displays the form again with error messages.
-postPersonR :: Handler Html
-postPersonR = do
-    ((result, widget), enctype) <- runFormPost personForm
+postLoginR :: Handler Html
+postLoginR = do
+    ((result, widget), enctype) <- runFormPost userForm
     case result of
-        FormSuccess person -> defaultLayout [whamlet|<p>#{show person}|]
+        FormSuccess user -> defaultLayout [whamlet|<p>Login Result:<p>#{show user}|]
         _ -> defaultLayout
             [whamlet|
-                <h1>Uh oh, something went wrong with the POST request.</h1>
+                <h1>Uh oh, something went wrong with the Login POST request.</h1>
                 <p>Invalid input, let's try again.
-                <form method=post action=@{PersonR} enctype=#{enctype}>
+                <form method=post action=@{LoginR} enctype=#{enctype}>
                     ^{widget}
                     <button>LoginAttempt2
             |]
 
+postAccRegisterR :: Handler Html
+postAccRegisterR = do
+    ((result, widget), enctype) <- runFormPost newAccountForm
+    case result of
+        FormSuccess newPerson -> defaultLayout [whamlet|<p>Register Result:<p>#{show newPerson}|]
+        _ -> defaultLayout
+            [whamlet|
+                <h1>Uh oh, something went wrong with the Registration POST request.</h1>
+                <p>Invalid input, let's try again.
+                <form method=post action=@{AccRegisterR} enctype=#{enctype}>
+                    ^{widget}
+                    <button>RegisterAttempt2
+            |]
+
 openConnectionCount :: Int
 openConnectionCount = 10
-
 main :: IO ()
 main = runStderrLoggingT $ withSqlitePool "test.db3" openConnectionCount
   $ \pool -> liftIO $ do
     runResourceT $ flip runSqlPool pool $ do
         runMigration migrateAll
-        insert $ Person "Michael" "Michaelspassword" 26
-    warp 3000 App pool
+        insert $ Person "Michael" "Snoyman" 26
+    warp 3000 $ PersistTest pool
